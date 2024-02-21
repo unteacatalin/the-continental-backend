f663767e73633de8ed87bb2c59fec2c88dde40a7
Catalin Untea <catalin.untea@gmail.com>
2024-02-21 11:57:31 +0200

    changed getRooms apiRooms

--- src/services/apiRooms.js
+++ src/services/apiRooms.js
@@ -2,18 +2,7 @@ import supabase, { supabaseUrl } from './supabase';
 import axios from 'axios';
 
 export async function getRooms() {
-  // let { data: rooms, error } = await supabase.from('rooms').select('*');
-
-  const { data: rooms, error } = await axios.get(
-    'https://untea-the-continental-backend-b7b62ca8f70a.herokuapp.com/api/v1/rooms',
-    {
-      withCredentials: true,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-      // credentials: 'same-origin',
-    }
-  );
+  let { data: rooms, error } = await supabase.from('rooms').select('*');
 
   // const { data: rooms, error } = await result.json();
 
