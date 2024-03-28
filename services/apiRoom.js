02930533ef0c6fb05c6b1669b902417e92a45d62
Catalin Untea <catalin.untea@gmail.com>
2024-03-28 15:56:30 +0200

    implemented create/update room

--- services/apiRoom.js
+++ services/apiRoom.js
@@ -140,7 +140,7 @@ exports.uploadImage = async function(req) {
       });
 
       // memStream.write(Buffer.from(file, 'base64'));
-      console.log({file: file.dataFile});
+      console.log({file: file?.image});
       // memStream.write(file.getImage());
 
       memStream.on('end', async function() {
