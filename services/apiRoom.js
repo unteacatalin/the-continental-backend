582c5707fa9398a77f678ddd3c7dff5c1d932348
Catalin Untea <catalin.untea@gmail.com>
2024-04-09 15:42:09 +0300

    implemented create/update room

--- services/apiRoom.js
+++ services/apiRoom.js
@@ -219,6 +219,7 @@ exports.createEditRoom = async function ({ newRoom, id }) {
 // };
 
 const parseFile = function(req) {
+  console.log(req?.file);
   const buffer = req?.file?.buffer;
   const fileName = req?.file?.filename;
   const mimeType = req?.file?.mimetype;
